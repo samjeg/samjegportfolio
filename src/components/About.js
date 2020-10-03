5471c1fc87593f3d2e6cb05787e46b43d85b254b
Samuel Jegede <sam.jegede@outlook.com>
2020-10-03 19:03:51 +0100

    basic ui for about page done

--- src/components/About.js
+++ src/components/About.js
@@ -8,26 +8,40 @@ class About extends React.Component {
 
 	render () {
 		return (
-			<section className="title_component">
-				<h1 className="page_title">
-				    About Me
-				</h1>
-			</section>
-			<section className="description_component">
-				<h2 className="description_title">
-				    Who I am
-				</h2>
-				<p className="description_intro">
-					My name is Sam, I'm training to be a Software Developer, I live London, UK
-				</p>
-				<p className="description_desc">
-					Always working on software project to build my software engineering skills 
-					and researching new information about software engineering to expand my current knowlegde   
-				</p>
-				<p className="description_project">
-					My name is Sam, I'm training to be a Software Developer, I live London, UK
-				</p>
-			</section>
+			<div>
+				<section className="title_component">
+					<h1 className="page_title">
+					    About Me
+					</h1>
+				</section>
+				<section className="desc_component">
+				    <h2 className="desc_title">
+				        Who I am
+				    </h2>
+					<ul className="desc_list">  
+						<li><p>My name is Sam, I'm training to be a Software Developer, I live London, UK</p></li>
+	                    <li><p className="desc_desc">
+	                            Always working on software project to build my software engineering skills
+	                            and researching new information about software engineering to expand my current knowlegde
+	                        </p>
+	                    </li>
+						<li><p>My name is Sam, I'm training to be a Software Developer, I live London, UK</p></li>
+					</ul>
+				</section>
+				<section className="adv_component">
+				    <h2 className="adv_title">
+				        Why work with me
+				    </h2>
+					<ul className="adv_list">  
+						<li><p>My name is Sam, I'm training to be a Software Developer, I live London, UK</p></li>
+	                    <li><p>
+	                            Always working on software project to build my software engineering skills
+	                            and researching new information about software engineering to expand my current knowlegde
+	                        </p>
+	                    </li>
+					</ul>
+				</section>		
+			</div>
 		);
 	}
 }
